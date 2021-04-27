8d4018023423b0e0347df619a59e7555a0a95369
Vinicius <vinicius.gl098@gmail.com>
2021-04-27 01:56:42 -0300

    DataPak script changed
    Script format
    
    Infra (#16)
    
    DataPak script changed
    
    * Script format
    
    Update git_release_pipeline.sh
    
    Update README.md
    
    Random Forest Experiment
    
    script fixes

---
+++ .infra/.terraform/output.tf
@@ -0,0 +1,11 @@
+output "cos_crn" {
+  value = ibm_resource_instance.cos.crn
+}
+
+output "wml_name" {
+  value = ibm_resource_instance.wml.name
+}
+
+output "wml_crn" {
+  value = ibm_resource_instance.wml
+}
