5f5038273b50a7a297c6ff5500890e9b348b4c6f
Guilherme Leite <guipleite@gmail.com>
2021-05-13 23:35:23 -0300

    Update git_release_pipeline.sh

--- src/scripts/Scripts/git_release_pipeline.sh
+++ src/scripts/Scripts/git_release_pipeline.sh
@@ -4,6 +4,7 @@ if  ! python3 ./src/scripts/Pipelines/git_release_pipeline.py ./
 then 
     echo "      Model already has been deployed, updating it"
     python3 ./src/scripts/Pipelines/model_update_pipeline.py ./models/model.joblib ./ ./credentials.yaml
+    python3 ./src/scripts/Pipelines/model_update_deployment_pipeline.py ./ ./credentials.yaml
 else    
     echo "      Deploying model for the first time" 
     python3 ./src/scripts/Pipelines/model_deploy_pipeline.py ./models/model.joblib ./ ./credentials.yaml
